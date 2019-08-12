2d936f413de3ae7a24a3dd3630177f06a911d8f4
Filipe Oliveira <filipe.soares@ans.gov.br>
2019-08-12 11:01:24 -0300

    Add Swagger configuration and Swagger-ui

--- src/main/java/br/com/springboot/rest/configuration/SwaggerConfiguration.java
+++ src/main/java/br/com/springboot/rest/configuration/SwaggerConfiguration.java
@@ -1,26 +1,29 @@
 package br.com.springboot.rest.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import br.com.springboot.rest.model.User;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
-	
+
 	@Bean
 	public Docket docketApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("br.com.springboot.rest"))
-				.paths(PathSelectors.ant("/**"))
-				.build();
-				// .ignoredParameterTypes(User.class);
-		
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.springboot.rest")).paths(PathSelectors.ant("/**"))
+				.build()
+				.globalOperationParameters(
+						Arrays.asList(new ParameterBuilder().name("Authorization").description("Header for auth")
+								.modelRef(new ModelRef("string")).parameterType("header").required(false).build()));
+
 	}
 
 }
